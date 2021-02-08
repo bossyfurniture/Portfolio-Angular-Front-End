89125e48a06567e17a5e8b9e8ccad36b6d49f449
Abbas Furniturewalla <abbas.furniturew@ufl.edu>
2021-02-08 14:53:27 -0500

    added some Router Links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginboxComponent } from './loginbox/loginbox.component';
+import { Routes, RouterModule, ROUTES } from '@angular/router';
+import { HomeComponent as HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path:'', component: HomeComponent},
+  { path:'loginbox', component: LoginboxComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginboxComponent
+    LoginboxComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
